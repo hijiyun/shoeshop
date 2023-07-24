090b5ed99c84fa8da4be205f18141d02c96560c9
박지윤 <tkrhkfld123@naver.com>
2023-07-24 10:20:36 +0900

    cartpage , Errorpage Update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import { Routes, Route, Link} from "react-router-dom"
 import MainPage from './components/MainPage';
 import Header from './components/Header';
 import Detail from './components/DetailPage';
-
+import Cart from './components/CartPage'
+import Error from './components/Error'
 
 
 function App() {
@@ -18,7 +19,8 @@ function App() {
     <Routes>
       <Route path='/' element={<MainPage />} />
       <Route path='/detail' element={<Detail />}/>
-      <Route path='/cart' element={<div>장바구니 페이지임</div>}/>
+      <Route path='/cart' element={<Cart />}/>
+      <Route path='*' element={<Error />} />
     </Routes>
     </div>
   );
