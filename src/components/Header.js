999522fae913a6ad8919a9687cd96b1aa6d68de5
박지윤 <tkrhkfld123@naver.com>
2023-07-24 10:21:10 +0900

    navigate 사용

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,19 @@
 import React from 'react'
-import {Navbar, Container, Nav} from "react-bootstrap"
-import {Link} from "react-router-dom"
+import {Navbar, Container, Nav, Card} from "react-bootstrap"
+import {Link, useNavigate} from "react-router-dom"
+import Cart from './CartPage'
 
 const Header = () => {
+
+  let navigate = useNavigate();
+
   return (
     <Navbar bg="light" data-bs-theme="light">
         <Container>
-          <Navbar.Brand href="#home">shoeshoe shop</Navbar.Brand>
+          <Navbar.Brand onClick={()=>{navigate('./')}}>shoeshoe shop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Cart</Nav.Link>
+            <Nav.Link onClick={()=>{navigate('./')}} >Home</Nav.Link>
+            <Nav.Link onClick={()=>{navigate('./cart')}}>Cart</Nav.Link>
           </Nav>
           <div className='page-change'>
             <Link className='home' to="/">Home</Link>
