c70f076ae2258bd994ce3c489942d4762deed9cf
박지윤 <tkrhkfld123@naver.com>
2023-07-28 23:12:49 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,28 +8,27 @@ import MainPage from './components/MainPage';
 import Header from './components/Header';
 import Detail from './components/DetailPage';
 import Cart from './components/CartPage'
-import Error from './components/Error'
 import Event from './components/Event';
 import data from "./data"
 
 
 
 function App() {
-  let [shoes] = useState(data)
+  let [shoes, setShoes] = useState(data)
   let [img] = useState([img1,img2,img3])
 
   return (
     <div className="App">
       <Header />
     <Routes>
-      <Route path='/' element={<MainPage shoes={shoes}/>} />
+      <Route path='/' element={<MainPage shoes={shoes} setshoes={setShoes}/>} />
       <Route path='/detail/:id' element={<Detail shoes={shoes} img={img}/>}/>
       <Route path='/cart' element={<Cart />}/>
       <Route path='/event' element={<Event />}>
         <Route path='one' element={ <p>첫 주문시 야배추즙 서비스</p>}/>
         <Route path='two' element={ <p>생일 기념 쿠폰받기</p>}/>
       </Route>
-      <Route path='*' element={<Error />} />
+
     </Routes>
     </div>
   );
