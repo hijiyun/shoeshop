3a262ec2f679a0309a48d709fc1b41686a637a83
박지윤 <tkrhkfld123@naver.com>
2023-07-31 15:55:02 +0900

    localStorage Update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import img1 from "./img/img1.webp"
 import img2 from "./img/img2.webp"
 import img3 from "./img/img3.webp"
-import { Routes, Route, Link} from "react-router-dom"
+import { Routes, Route, Link, json} from "react-router-dom"
 import MainPage from './components/MainPage';
 import Header from './components/Header';
 import Detail from './components/DetailPage';
@@ -14,6 +14,14 @@ import data from "./data"
 
 
 function App() {
+  let obj = {name: 'jiyun'}
+  localStorage.setItem('data', JSON.stringify(obj)) // 왼쪽이 작명, 오른쪽이 넣고 싶은 데이터
+  let data = localStorage.getItem('data')
+  let out = JSON.parse(data)
+  console.log(out)
+
+
+
   let [shoes, setShoes] = useState(data)
   let [img] = useState([img1,img2,img3])
 
