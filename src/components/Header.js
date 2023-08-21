cd7b620d5b87b2dff899d980410c7d22acadb6f2
박지윤 <tkrhkfld123@naver.com>
2023-08-22 00:30:45 +0900

    Header.js react-query로 get 요청하기

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,23 @@ import React from 'react'
 import {Navbar, Container, Nav, Card} from "react-bootstrap"
 import {Link, useNavigate} from "react-router-dom"
 import Cart from './CartPage'
+import axios from 'axios'
+import { useQuery } from 'react-query'
 
 const Header = () => {
 
   let navigate = useNavigate();
+
+  // [ axios썻을 때 ]
+  // axios.get('https://codingapple1.github.io/userdata.json').then((a)=>{
+  //   console.log(a.data.name)
+  // })
+
+  // [ react-query 썻을 때 ]
+  let result = useQuery('작명', ()=>{
+    return axios.get('https://codingapple1.github.io/userdata.json').then((a)=> a.data)
+  })
+
 
   return (
     <Navbar bg="light" data-bs-theme="light">
@@ -14,6 +27,9 @@ const Header = () => {
           <Nav className="me-auto">
             <Nav.Link onClick={()=>{navigate('./')}} >Home</Nav.Link>
             <Nav.Link onClick={()=>{navigate('./cart')}}>Cart</Nav.Link>
+          </Nav>
+          <Nav className="me-auto">
+            {result.isLoading ? '로딩중' : '반가워요  '+result.data.name}
           </Nav>
           <div className='page-change'>
             <Link className='home' to="/">Home</Link>
